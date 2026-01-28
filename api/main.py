d8316a01ad62e2a57aae2de855336f1d60d5b39f
adolfosalasgomez3011 <adolfosalasgomez@gmail.com>
2026-01-28 01:03:40 -0500

    Expose Content-Disposition header for CORS to enable custom filename download

--- api/main.py
+++ api/main.py
@@ -56,6 +56,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["Content-Disposition"],  # Allow frontend to read this header
 )
 
 # Initialize services
